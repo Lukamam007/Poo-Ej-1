5232390b3ba4c8ac51b3193ede9a6cef717fbf43
Lukamam007 <lukamam007@gmail.com>
2026-04-21 15:27:14 -0300

    s

--- IJugador.cs
+++ IJugador.cs
@@ -2,6 +2,7 @@ namespace Jugadores{
 
     public interface Ijugador{
 
+      public int tiempo{get;set;}
       public bool Cansado();
       public void Correr (int minutos);
       public void Descansar (int minutos);
